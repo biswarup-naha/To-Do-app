bdeeb43f2496bd3ba62cf070c367e8060b09e371
biswarup-naha <jeeeeet6902@gmail.com>
2024-03-30 21:58:00 +0530

    basic ui made

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    <div className="max-w-2xl m-auto py-0 px-1">
+      <h1 className="text-6xl font-bold text-center mt-4">To Do App</h1>
+      <div className="flex justify-center items-center mt-8 gap-4">
+        <input type="text" className="w-96 py-0 px-2 border-solid border-slate-500 border-b outline-0" placeholder='Enter your to do here...' />
+        <div className=' bg-black text-white py-1 px-5'>Add Task</div>
+      </div>
+     
+    </div>
+
+    </div>
+  )
+}
+
+export default App
